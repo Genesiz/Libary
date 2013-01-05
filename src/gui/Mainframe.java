b07d39b5d753a506fa70472d450c7ed5e3d442ad
Hjalte J <skjoldborg94@edb.dk>
2013-01-05 22:08:27 +0100

    lol

--- src/gui/Mainframe.java
+++ src/gui/Mainframe.java
@@ -1,24 +1,11 @@
 package gui;
 
 
-import java.awt.BorderLayout;
-import java.awt.ComponentOrientation;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.SpringLayout;
+import javax.swing.*;
 
 import library.Archieve;
 
@@ -48,7 +35,7 @@ public class Mainframe extends JFrame {
 		 jcbGenre = new JCheckBox("Genre");
 		 jcbLength = new JCheckBox("Length");
 		 jcbRating = new JCheckBox("Rating");
-		 		 
+		 		 		 
 		 jbtNew.addActionListener(new ActionListener() {
 
 			@Override
