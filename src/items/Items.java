b5df0a4181ad688a5325f294f64dbc3c8931008c
Torhus <Torhus@GamingT>
2013-01-05 15:28:30 +0100

    Lol

---
+++ src/items/Items.java
@@ -0,0 +1,15 @@
+package items;
+
+import java.util.Arrays;
+
+public abstract class Items {
+	
+	public abstract String GetTitle();
+	public abstract String GetAuthor();
+	public abstract double GetLength();
+	public abstract String GetGenre();
+	public abstract int GetRating();
+	public abstract String GetType();
+	
+
+}
