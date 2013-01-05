b248757a533168353914f687564d81658e25bd07
Torhus <Torhus@GamingT>
2013-01-05 16:23:44 +0100

    lol

--- src/Test.java
+++ src/Test.java
@@ -13,12 +13,12 @@ public class Test {
 	public static void main(String[] args) {
 		Archieve local = Archieve.library;
 		try {
-			local.addItem(new Books("title", "author", 
-					300, "genre", 4, "BOOK"));
-			local.addItem(new Books("title2", "author", 
-					300, "genre", 4, "BOOK"));
-			local.addItem(new Books("title3", "author", 
-					300, "genre", 4, "BOOK"));
+			local.addItem(new Books("Lorte Sang", "Bjarne", 
+					2.42, "Classic", 1, "MUSIC"));
+			local.addItem(new Books("God bog", "Mr. Awesome", 
+					632, "Fantasy", 5, "BOOK"));
+			local.addItem(new Books("Lorte bog", "Mr. Nitte", 
+					30, "Political", 0, "BOOK"));
 		} catch (IllegalItemException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
