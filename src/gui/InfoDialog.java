acc7a3b3fda3e719e163265b2d6a45b98f97dddc
Hjalte <Hjalte@Hjalte-Home-PC>
2013-01-10 03:44:43 +0100

    Info dialog on click and working delete button

--- src/gui/InfoDialog.java
+++ src/gui/InfoDialog.java
@@ -1,14 +1,22 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import items.Item;
 import items.Item.ItemType;
 import javax.swing.*;
+
+import library.Archive;
 
 public class InfoDialog extends JDialog {
 	
 	private static final long serialVersionUID = 1L;
 
-	public InfoDialog(MainFrame frame, Item item) {
+	public InfoDialog(MainFrame frame, Item item, final int index) {
 		super(frame, true);
 		JPanel p = new JPanel();
 		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
@@ -23,13 +31,42 @@ public class InfoDialog extends JDialog {
 			jlLength = new JLabel("Pages: " + item.getLength());
 		
 		JLabel jlRating = new JLabel("Rating: " + item.getRating());
+		JButton jbDelete = new JButton("Delete");
+		JButton jbCancel = new JButton("Cancel");
+		
+		jbDelete.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Archive.library.getLibrary().remove(index);
+				ListPanel.updateList();
+				dispose();
+			}
+			
+		});
+		
+		jbCancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+			
+		});
+		
 		p.add(jlTitle);
 		p.add(jlAuthor);
 		p.add(jlGenre);
 		p.add(jlLength);
 		p.add(jlRating);
-		this.add(p);
+		
+		JPanel p2 = new JPanel();
+		p2 .add(jbDelete);
+		p2.add(jbCancel);
+
+		this.setLayout(new BorderLayout());
+		this.add(p, BorderLayout.PAGE_START);
+		this.add(p2, BorderLayout.PAGE_END);
 		this.pack();
 		if (item.getType() == (ItemType.MUSIC))
 			this.setTitle("Music");
--- src/gui/ListPanel.java
+++ src/gui/ListPanel.java
@@ -31,11 +31,14 @@ public class ListPanel extends JPanel {
 		    	   return false;
 		    }
 		};
-		model.setColumnCount(Archive.library.columnNames.length);
+		int columns = Archive.library.columnNames.length;
+		model.setColumnCount(columns);
 		model.setRowCount(Archive.library.getLibrary().size());
 		
 		JLabel jLabel = new JLabel("Search result:");
 		jlSearchResult = new JTable(model);
+		jlSearchResult.getColumnModel().getColumn(columns - 1).setPreferredWidth(10);
+		jlSearchResult.getColumnModel().getColumn(columns - 2).setPreferredWidth(10);
 
 		ListSelectionModel selectionModel = jlSearchResult.getSelectionModel();  
         selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);  
@@ -47,28 +50,32 @@ public class ListPanel extends JPanel {
 	}
 	
 	public static void updateList() {
-		model.setRowCount(Archive.library.getLibrary().size());
+		int size = Archive.library.getLibrary().size();
+		model.setRowCount(size);
 		
-		for (int i = 0; i < Archive.library.getLibrary().size(); i++) {
-			model.setValueAt(Archive.library.getItemAt(i).getTitle(), i, 0);
-			model.setValueAt(Archive.library.getItemAt(i).getAuthor(), i, 1);
-			model.setValueAt(Archive.library.getItemAt(i).getGenre(), i, 2);
-			model.setValueAt(Archive.library.getItemAt(i).getLength(), i, 3);
-			model.setValueAt(Archive.library.getItemAt(i).getRating(), i, 4);
+		for (int i = 0; i < size; i++) {
+			Item item = Archive.library.getItemAt(i);
+			model.setValueAt(item.getTitle(), i, 0);
+			model.setValueAt(item.getAuthor(), i, 1);
+			model.setValueAt(item.getGenre(), i, 2);
+			model.setValueAt(item.getLength(), i, 3);
+			model.setValueAt(item.getRating(), i, 4);
 		}	
 		jlSearchResult.setModel(model);
 
 	}
 	
 	public static void updateList(Item[] array) {
-		model.setRowCount(array.length);
+		int size = array.length;
+		model.setRowCount(size);
 
-		for (int i = 0; i < array.length; i++) {
-			model.setValueAt(array[i].getTitle(), i, 0);
-			model.setValueAt(array[i].getAuthor(), i, 1);
-			model.setValueAt(array[i].getGenre(), i, 2);
-			model.setValueAt(array[i].getLength(), i, 3);
-			model.setValueAt(array[i].getRating(), i, 4);
+		for (int i = 0; i < size; i++) {
+			Item item = array[i];
+			model.setValueAt(item.getTitle(), i, 0);
+			model.setValueAt(item.getAuthor(), i, 1);
+			model.setValueAt(item.getGenre(), i, 2);
+			model.setValueAt(item.getLength(), i, 3);
+			model.setValueAt(item.getRating(), i, 4);
 		}
 		jlSearchResult.setModel(model);
 	}
@@ -79,8 +86,10 @@ public class ListPanel extends JPanel {
 		public void valueChanged(ListSelectionEvent arg0) {
 			if (arg0.getValueIsAdjusting()) {
 				int index = jlSearchResult.getSelectedRow();
+				if (index != -1) {
 				Item item = Archive.library.getItemAt(index);
-				new InfoDialog(MainFrame.frame, item);
+				new InfoDialog(MainFrame.frame, item, index);
+				}
 			}
 		}
 		
