68eddfa210102c8d9c0673b5855038d44f7a1c06
Torhus <Torhus@GamingT>
2013-01-12 17:44:14 +0100

    Load button & listener

---
+++ src/io/LoadListener.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class LoadListener extends JFileChooser implements ActionListener  {
+
+		public LoadListener(){
+			
+		}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("LIB file ", "lib");
+		this.setFileFilter(filter);
+		int returnVal = this.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+			new Load(this.getSelectedFile().getAbsolutePath(), false);
+	}
+
+}
